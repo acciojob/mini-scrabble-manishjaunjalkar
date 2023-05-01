7ffc87d5ea54818ac047cf661ed1367e6238dfed
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-05-02 00:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let count = document.getElementById("evaluatedText");
+
+count.addEventListener("input",counttext);
+
+function counttext(){
+let a=count.value.trim().length;
+document.getElementById("letterCount").innerText = a;
+}
